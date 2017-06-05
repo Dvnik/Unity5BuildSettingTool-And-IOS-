48dc9f3b67e67be22ae90dff5cecd8cc91ced170
dvnik <dvnik@local.low>
2017-06-05 17:47:42 +0800

    紀錄EditorGUILayout的使用效果，未來修改版面用

--- Assets/SGamesTools/Editor/TestFile.cs
+++ Assets/SGamesTools/Editor/TestFile.cs
@@ -1,9 +1,12 @@
-﻿using UnityEngine;
+﻿#if UNITY_EDITOR
+using UnityEngine;
 using UnityEditor;
 using System.Collections;
 using System.IO;
-
-public class TestFile
+#region BeginFadeGroup
+//using UnityEditor.AnimatedValues;
+#endregion
+public class TestFile : EditorWindow
 {
 //	[MenuItem("SuperGame/Test/UnityLogShow")]
 	private static void FilePathLog()
@@ -19,10 +22,10 @@ public class TestFile
 		Debug.Log("Remove AssetsPath\n" +aPath);
 
 	}
-//	[MenuItem("SuperGame/Test/UnityLogShow")]
+	[MenuItem("SuperGame/Test/TestGUI")]
 	private static void TestFunction()
 	{
-
+		EditorWindow.GetWindow(typeof(TestFile));
 	}
 //	[MenuItem("SuperGame/Test/UnityLogShow")]
 	private static void ProTest()
@@ -49,6 +52,107 @@ public class TestFile
 //		Debug.Log(eMobileSplashScreen.iPhoneSplashScreen.ToString());
 	}
 
+//	private void OnEnable()
+//	{
+//
+//	}
+
+
+
+	private void OnGUI()
+	{
+
+	}
+	#region UI Text
+//	bool showBtn = true;
+//	/// <summary>
+//	/// Examps the toggle.
+//	/// </summary>
+//	private void ExampToggle()
+//	{
+//		showBtn = EditorGUILayout.Toggle("Show Button", showBtn);
+//
+//		showBtn = EditorGUILayout.ToggleLeft("Left Show Btn", showBtn);
+//
+//
+//		if (showBtn)
+//		if (GUILayout.Button("Close"))
+//			this.Close();
+//	}
+//	string ammo;
+//	string ammo2;
+//	/// <summary>
+//	///	PerfixLabel和 LabelField的差別在視窗分配上
+//	///	PerfixLabel : 只取用一小部分，其餘是輸入區域
+//	///	LabelField : 和輸入區域對分一半使用區
+//	/// </summary>
+//	private void ExampPrefixLabel()
+//	{
+//		EditorGUILayout.BeginHorizontal();
+//		EditorGUILayout.PrefixLabel("Ammo");
+//		ammo = EditorGUILayout.TextField(ammo);
+//		EditorGUILayout.EndHorizontal();
+//		EditorGUILayout.Space();// 留個空間
+//		EditorGUILayout.BeginHorizontal();
+//		EditorGUILayout.LabelField("Ammo2");
+//		ammo2 = EditorGUILayout.TextField(ammo2);
+//		EditorGUILayout.EndHorizontal();
+//	}
+	/// <summary>
+	/// 在GUI裡面出現提示訊息用的
+	/// </summary>
+//	private void ExampHelpBox()
+//	{
+//		EditorGUILayout.HelpBox("1111", MessageType.Info);
+//	}
+//	#endregion
+//	#region Foldout
+//	bool mShowPosition = true;
+//	string mStatus = "Select a GameObject";
+//	Vector3 mField = new Vector3();
+//	private void ExampFoldout()
+//	{
+//		mShowPosition = EditorGUILayout.Foldout(mShowPosition, mStatus);
+//		if (mShowPosition) {
+//			mField = EditorGUILayout.Vector3Field("Position", mField);
+//		}
+//	}
+	#endregion
+
+	#region BeginFadeGroup
+//	AnimBool mShowExtraF;
+//	string mString;
+//	Color mColor = Color.white;
+//	int mNum = 0;
+//
+//	private void ExampFadeGroupInit()
+//	{
+//		mShowExtraF = new AnimBool(true);
+//		mShowExtraF.valueChanged.AddListener(Repaint);
+//	}
+//
+//	/// <summary>
+//	/// 用動畫的方式顯示內容物
+//	/// </summary>
+//	private void ExampFadeGroup()
+//	{
+//		mShowExtraF.target = EditorGUILayout.ToggleLeft("Show extra fields", mShowExtraF.target);
+//		//Extra block that can be toggled on and off.
+//		if (EditorGUILayout.BeginFadeGroup(mShowExtraF.faded))// Start
+//		{
+//			EditorGUI.indentLevel++;
+//			EditorGUILayout.PrefixLabel("Color");
+//			mColor = EditorGUILayout.ColorField(mColor);
+//			EditorGUILayout.PrefixLabel("Text");
+//			mString = EditorGUILayout.TextField(mString);
+//			EditorGUILayout.PrefixLabel("Number");
+//			mNum = EditorGUILayout.IntSlider(mNum, 0, 10);
+//			EditorGUI.indentLevel--;
+//		}
+//		EditorGUILayout.EndFadeGroup();// End
+//	}
+	#endregion
 
 
 }
+#endif
